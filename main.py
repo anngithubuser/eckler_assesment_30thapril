f73220f7fbaed4bd9545ca673c3c2dde07f3fe8a
anngithubuser <angelica.e2001@gmail.com>
2024-04-30 11:11:55 +0200

    submit assesment 30th april 2024

--- main.py
+++ main.py
@@ -2,14 +2,45 @@
 Simple Calculator
 """
 # Provide your solution here
+a=int(input(""))
+b=int(input(""))
+x=input("")
+
+def calculate(a, b, x):
+    if x== "+":
+        return a+b
+    elif x == "-":
+        return a-b
+    elif x== "*":
+        return a*b
+    elif x== "/":
+        return a%b
+        if b== "0":
+            print("Division by 0 is not allowed.")
+    else:
+        print("Invalid operator")
+
+result=(calculate(a,b,x))
+print(result)
 
 
+
+"""
+result = add(5, 6)
+print(result)
+"""
 """
 Reverse Word
 """
 # Provide your solution here
-
+"""
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
 
+word=input("")
+def reverse_word(word):
+  return word[::-1]
+cap=reverse_word(word).capitalize()
+print(cap)
+"""
